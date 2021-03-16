6cd9ce7ca91c85cffa8db1e715e11ee5da3d9714
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2021-03-16 15:11:14 +0100

    Initial release

--- src/components/CustomGroupBox.tsx
+++ src/components/CustomGroupBox.tsx
@@ -25,36 +25,64 @@ export class CustomGroupBox extends Component<CustomGroupBoxProps, State> {
     }
 
     render(): ReactNode {
-        const { collapsible, headerContent, headerPreviewContent, bodyContent, bodyPreviewContent } = this.props;
+        const { collapsible } = this.props;
         // When the widget is first rendered, state will be at initial value.
         // Determine the state to use from the properties and set the state.
         // Do not render content yet; widget will render again.
         if (this.state.boxStatus === "initial") {
             if (collapsible === "yesStartCollapsed") {
-                console.info("CustomGroupBox.render: set state collapsed only");
+                // console.info("CustomGroupBox.render: set state collapsed only");
                 this.setState({ boxStatus: "collapsed" });
             } else {
-                console.info("CustomGroupBox.render: set state expanded only");
+                // console.info("CustomGroupBox.render: set state expanded only");
                 this.setState({ boxStatus: "expanded" });
             }
             return null;
         }
 
-        console.info("CustomGroupBox.render: render content");
+        // console.info("CustomGroupBox.render: render content");
 
-        let className = "mx-groupbox " + this.props.class;
+        let containerClassName = "mx-groupbox " + this.props.class;
         if (collapsible !== "no") {
-            className += " mx-groupbox-collapsible";
+            containerClassName += " mx-groupbox-collapsible";
         }
-        className += " " + this.state.boxStatus;
+        containerClassName += " " + this.state.boxStatus;
+        const { headerContent, headerPreviewContent, bodyContent, bodyPreviewContent } = this.props;
         return (
-            <div className={className}>
-                <div className="mx-groupbox-header">{this.getContent(headerContent, headerPreviewContent)}</div>
+            <div className={containerClassName}>
+                <div className="mx-groupbox-header" onClick={() => this.handleClick()}>
+                    <div className="customGroupBoxHeaderContainer">
+                        <div className="customGroupBoxHeaderContent">
+                            {this.getContent(headerContent, headerPreviewContent)}
+                        </div>
+                        {this.getIcon()}
+                    </div>
+                </div>
                 <div className="mx-groupbox-body">
                     {this.state.boxStatus ? this.getContent(bodyContent, bodyPreviewContent) : null}
                 </div>
             </div>
         );
+    }
+
+    handleClick(): void {
+        if (this.props.collapsible === "no") {
+            return;
+        }
+
+        this.setState((currentState: State) => {
+            return { boxStatus: currentState.boxStatus === "expanded" ? "collapsed" : "expanded" };
+        });
+    }
+
+    getIcon(): ReactNode {
+        if (this.props.collapsible === "no") {
+            return null;
+        }
+
+        const iconName = this.state.boxStatus === "expanded" ? "minus" : "plus";
+        const iconClassName = "glyphicon mx-groupbox-collapse-icon glyphicon-" + iconName;
+        return <i className={iconClassName} />;
     }
 
     getContent(content?: ReactNode, contentPreview?: { widgetCount: number; renderer: ComponentType }): ReactNode {
